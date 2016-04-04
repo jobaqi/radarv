b442020e7af3c1a9d1f0695a07601c3e19f9894f
Jordy van Domselaar <Jordy van Domselaar>
2016-04-04 14:39:58 +0200

    Add dev-server, fix bug

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -17,14 +17,14 @@ class HomeController extends Controller{
     /**
      * Show index page
      */
-    public function pvi(){
+    public static function pvi(){
         return self::renderWithLayout("pvi", ['title' => 'RadarV-index']);
     }
     
     /**
      * Show analysis page
      */
-    public function analysis(){
+    public static function analysis(){
         return self::renderWithLayout('analysis', ['title' => 'RadarV-analysis']);
     }
 }
