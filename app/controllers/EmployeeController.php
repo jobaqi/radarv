aff1d59fb0565ad1b41524a98673d54e2bb207e3
Kipsh0arma <q.wieling@gmail.com>
2016-04-19 14:10:14 +0200

    Update
    fixed pvi routes

--- app/controllers/EmployeeController.php
+++ app/controllers/EmployeeController.php
@@ -9,12 +9,12 @@ class EmployeeController extends Controller{
      */
     public static function index(){
         $erepo = new \App\Repositories\EmployeeRepository();
-        return self::renderWithLayout("employee", ['title' => 'EmployeeOverView', 'employees' => $erepo->getAll()]);
+        return self::renderWithLayout("employee", ['title' => 'Medewerkers overzicht', 'employees' => $erepo->getAll()]);
     }
      /*
       * Show pvi page
       */
     public static function pvi(){
-        return self::renderWithLayout("pvi", ['title' => 'RadarV-PVI']);
+        return self::renderWithLayout("pvi", ['title' => 'Plan van inzet']);
     }
 }
